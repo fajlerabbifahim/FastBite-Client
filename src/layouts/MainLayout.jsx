e44554e09c457b5119d7460158c7101c96428424
Yourfaahim <fahimqw1@gmail.com.com>
2025-03-13 15:21:01 +0600

    main.jsx doubble import staticmode bug fix

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,18 +1,18 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
-    <div className=''>
+    <div className="">
       {/* <h1>hello</h1> */}
       {/* navbar */}
       {/* <div className='pt-24 min-h-[calc(100vh-68px)]'>
         <Outlet />
       </div> */}
-      <div >
+      <div>
         <Outlet />
       </div>
       {/* footer */}
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
