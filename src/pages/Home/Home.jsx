1d568ead0cdc5af60555639dbecbaef31309750f
amahim <arifbin71@gmail.com>
2025-03-13 13:53:20 +0600

    added footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Footer from '../../Components/Footer/footer'
 
 const Home = () => {
   return (
     <div>
       <h1 className='text-5xl text-red-500'>This is home page</h1>
+      <Footer/>
     </div>
   )
 }
